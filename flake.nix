3925cebd7b871baa40257814a8de069425d5f03b
Ian Manske <ian.manske@pm.me>
2026-08-05 18:37:14 -0700

    Update flake

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
         let rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml; in
         {
           devShells.default = with pkgs; mkShell {
-            packages = [ rustToolchain cargo-hack just typos ];
+            packages = [ rustToolchain rustup cargo-hack just typos ];
           };
         }
       );
